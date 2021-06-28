205d6e7d6327787fabf0cfb57ea2caca8e287765
Murilo Gomes <murilo.goomes@hotmail.com>
2021-06-28 17:50:01 -0300

    chore: add more tests

--- kudos.test.js
+++ kudos.test.js
@@ -4,9 +4,16 @@ test('test getKudosForUser', () => {
   expect(kudos.getKudosForUser(30)).toEqual(['GOOD', 'NICE']);
   expect(kudos.getKudosForUser(40)).toEqual(['GOOD', 'GOOD']);
   expect(kudos.getKudosForUser(100)).toEqual(['SUPER']);
+  expect(kudos.getKudosForUser(135)).toEqual(['SUPER', 'GOOD', 'NICE', 'OK'])
 });
 
 test('test getKudosValueMessageForUser', () => {
   expect(kudos.getKudosValueMessageForUser(kudos.getKudosForUser(30)))
     .toEqual('Você recebeu treze reais em retorno aos kudos GOOD, NICE!');
+  expect(kudos.getKudosValueMessageForUser(kudos.getKudosForUser(40)))
+    .toEqual('Você recebeu dezesseis reais em retorno aos kudos GOOD, GOOD!');
+  expect(kudos.getKudosValueMessageForUser(kudos.getKudosForUser(100)))
+    .toEqual('Você recebeu vinte e cinco reais em retorno aos kudos SUPER!');
+  expect(kudos.getKudosValueMessageForUser(kudos.getKudosForUser(135)))
+    .toEqual('Você recebeu quarenta reais em retorno aos kudos SUPER, GOOD, NICE, OK!');
 });
